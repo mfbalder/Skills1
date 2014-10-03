29e004a109e876dcc26cc4e177b8e3db4e5b9347
Micki <mfbalder@gmail.com>
2014-10-03 15:13:10 -0700

    Added the rest of the list comprehension versions

--- skills1.py
+++ skills1.py
@@ -10,10 +10,10 @@ def all_odd(number_list):
     ###### with list comprehension ######
     return [x for x in number_list if x % 2 != 0]
 
-    #### with filter function ######
+    #### with functional programming ######
     # return filter(lambda x: x % 2 != 0, number_list)
 
-    #### standard version ####
+    #### with iteration ####
     # new_list = []
     # for num in number_list:
     #     if x % 2 != 0:
@@ -26,10 +26,10 @@ def all_even(number_list):
     ###### with list comprehension ######
     return [x for x in number_list if x % 2 == 0]
 
-    #### with filter function ######
+    #### with functional programming ######
     # return filter(lambda x: x % 2 == 0, number_list)
 
-    #### standard version ####
+    #### with iteration ####
     # new_list = []
     # for num in number_list:
     #     if x % 2 == 0:
@@ -38,7 +38,13 @@ def all_even(number_list):
 
 # Write a function that takes a list of strings and returns a new list with all strings of length 4 or greater.
 def long_words(word_list):
-    return filter(lambda x: len(x) >= 4, word_list)
+    ###### with list comprehension ######
+    return [x for x in word_list if len(x) >= 4]
+    
+    #### with functional programming ######
+    # return filter(lambda x: len(x) >= 4, word_list)
+
+    #### with iteration ####
     # new_list = []
     # for word in word_list:
     #     if len(word) >= 4:
@@ -47,7 +53,10 @@ def long_words(word_list):
 
 # Write a function that finds the smallest element in a list of integers and returns it.
 def smallest(number_list):
+    #### with functional programming ######
     return reduce(lambda x, y: y if y < x else x, number_list)
+    
+    #### with iteration ####
     # smallest_index = 0
     # for number_index in range(len(number_list)):
     #     if number_list[number_index] > number_list[smallest_index]:
@@ -56,7 +65,10 @@ def smallest(number_list):
 
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(number_list):
+    #### with functional programming ######
     return reduce(lambda x, y: y if y > x else x, number_list)
+
+    #### with iteration ####
     # largest_index = 0
     # for number_index in range(len(number_list)):
     #     if number_list[number_index] > number_list[largest_index]:
@@ -66,7 +78,13 @@ def largest(number_list):
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
 def halvesies(number_list):
-    return map(lambda x: x / 2.0, number_list)
+    ###### with list comprehension ######
+    return [x / 2.0 for x in number_list]
+
+    #### with functional programming ######
+    # return map(lambda x: x / 2.0, number_list)
+
+    #### with iteration ####
     # new_list = []
     # for num in number_list:
     #     new_list.append(num / 2.0)
@@ -74,7 +92,12 @@ def halvesies(number_list):
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
 def word_lengths(word_list):
-    return map(lambda x: len(x), word_list)
+    return[len(x) for x in word_list if len(x) >= 4]
+
+    #### with functional programming ######
+    # return map(lambda x: len(x), word_list)
+    
+    #### with iteration ####
     # new_list = []
     # for word in word_list:
     #     new_list.append(len(word))
@@ -82,8 +105,10 @@ def word_lengths(word_list):
 
 # Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(number_list):
+    #### with functional programming ######
     return reduce(lambda x, y: x + y, number_list)
 
+    #### with iteration ####
     # sum = 0
     # for number in number_list:
     #     sum = sum + number
@@ -91,7 +116,10 @@ def sum_numbers(number_list):
 
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(number_list):
+    #### with functional programming ######
     return reduce(lambda x, y: x * y, number_list)
+
+    #### with iteration ####
     # total = 1
     # for number in number_list:
     #     total = total * number
@@ -99,7 +127,10 @@ def mult_numbers(number_list):
 
 # Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
 def join_strings(word_list):
+    #### with functional programming ######
     return reduce(lambda x, y: x + y, word_list)
+
+    #### with iteration ####
     # single_string = ""
     # for word in word_list:
     #     single_string = single_string + word
@@ -107,7 +138,10 @@ def join_strings(word_list):
 
 # Write a function that takes a list of integers and returns the average (without using the avg method)
 def average(number_list):
+    #### with functional programming ######
     return reduce(lambda x, y: x + y, number_list) / len(number_list)
+
+    #### with iteration ####
     # total = 0
     # count = 0
     # for number in number_list:
@@ -118,12 +152,12 @@ def average(number_list):
 
 def main():
     # print all_odd(number_list)
-    print all_even(number_list)
+    # print all_even(number_list)
     # print long_words(word_list)
     # print smallest(number_list)
     # print largest(number_list)
     # print halvesies(number_list)
-    # print word_lengths(word_list)
+    print word_lengths(word_list)
     # print sum_numbers(number_list)
     # print mult_numbers(number_list)
     # print join_strings(word_list)
